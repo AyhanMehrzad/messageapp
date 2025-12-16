44eb3f52282e6fe8d3ab59283f6321b2a8220b4d
rupper <mehrzad.mobin@gmail.com>
2025-12-16 09:07:24 +0100

    New design prototype

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = ({ user, isConnected }) => {
   return (
     <header className="app-header">
-      <div className="app-title">STITCH</div>
+      <div className="app-title">SecureChanel</div>
       <div className={`user-status ${isConnected ? 'connected' : ''}`}>
         {isConnected ? '● SECURE' : '○ CONNECTING...'}
       </div>
